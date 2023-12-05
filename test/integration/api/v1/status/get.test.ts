2704670efa1d34632eac53d38349d8517bb5e51a
surfacez <sergiobatistajr94@gmail.com>
2023-12-05 18:53:03 -0300

    Update API test for status endpoint

--- test/integration/api/v1/status/get.test.ts
+++ test/integration/api/v1/status/get.test.ts
@@ -3,6 +3,13 @@ describe("API Tests", () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
     expect(response.status).toBe(200);
     const body = await response.json();
-    expect(body).toBe("são tops");
+    expect(body.update_at).toBeDefined();
+    const parsedUpdatedAt = new Date(body.update_at).toISOString();
+    expect(body.update_at).toEqual(parsedUpdatedAt);
+    expect(body.dependecies.database.version).toBe("16.1");
+    expect(body.dependecies.database.max_connections).toBe("100");
+    expect(
+      Number(body.dependecies.database.current_connections),
+    ).toBeGreaterThanOrEqual(1);
   });
 });
